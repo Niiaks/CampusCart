431ae2e1e7d8558dd98877076e7b96acbd3093cf
Niiaks <napappoe007@st.ug.edu.gh>
2026-02-11 20:01:02 +0000

    feat: implement main function with server initialization and graceful shutdown

--- apps/server/cmd/campusCart/main.go
+++ apps/server/cmd/campusCart/main.go
@@ -1,8 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/Niiaks/campusCart/internal/config"
+	"github.com/Niiaks/campusCart/internal/logger"
+	"github.com/Niiaks/campusCart/internal/server"
+)
+
+const DefaultContextTimeout = 30
 
 func main() {
 
-	fmt.Println("campusCart api")
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		panic("failed to load config: " + err.Error())
+	}
+
+	// Initialize New Relic logger service
+	loggerService := logger.NewLoggerService(cfg.Observability)
+	defer loggerService.Shutdown()
+
+	log := logger.NewLoggerWithService(cfg.Observability, loggerService)
+
+	// Initialize server
+	srv, err := server.New(cfg, &log, loggerService)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to initialize server")
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	// Start server
+	go func() {
+		if err = srv.Start(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal().Err(err).Msg("failed to start server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), DefaultContextTimeout*time.Second)
+
+	if err = srv.Shutdown(ctx); err != nil {
+		log.Fatal().Err(err).Msg("server forced to shutdown")
+	}
+	stop()
+	cancel()
+
+	log.Info().Msg("server exited properly")
 }
