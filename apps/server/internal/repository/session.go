baf6505d8ae76d1844b968991ba6b055bb629533
Niiaks <napappoe007@st.ug.edu.gh>
2026-02-14 23:13:34 +0000

    feat: implement user and session repository with CRUD operations

---
+++ apps/server/internal/repository/session.go
@@ -0,0 +1,61 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/Niiaks/campusCart/internal/model"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type SessionRepository struct {
+	pool *pgxpool.Pool
+}
+
+type SessionRepo interface {
+	CreateSession(ctx context.Context, session *model.Session) error
+	GetUserBySession(ctx context.Context, sessionID string) (string, error)
+}
+
+func NewSessionRepository(pool *pgxpool.Pool) *SessionRepository {
+	return &SessionRepository{
+		pool: pool,
+	}
+}
+
+func (sr *SessionRepository) CreateSession(ctx context.Context, session *model.Session) error {
+
+	expiresAt := time.Now().Add(30 * time.Second)
+
+	sql := `INSERT INTO sessions (user_id, expires_at) VALUES($1,$2) returning id`
+
+	err := sr.pool.QueryRow(ctx, sql, session.UserID, expiresAt).Scan(&session.ID)
+
+	if err != nil {
+		return fmt.Errorf("Create session error %s", err)
+	}
+
+	return nil
+}
+
+func (sr *SessionRepository) GetUserBySession(ctx context.Context, sessionID string) (*model.User, error) {
+
+	sql := `SELECT u.id, u.email, u.role FROM sessions s INNER JOIN users u on s.user_id = u.id WHERE s.id = $1 AND expires_at > $2`
+
+	var user model.User
+
+	row := sr.pool.QueryRow(ctx, sql, sessionID, time.Now())
+
+	err := row.Scan(&user.ID, &user.Email, &user.Role)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, fmt.Errorf("Session not found %s", err)
+		}
+		return nil, err
+	}
+	return &user, nil
+}
--- apps/server/pkg/types/auth.go
+++ apps/server/pkg/types/auth.go
@@ -1,18 +1,23 @@
-package model
+package types
 
 import "time"
 
-// User defines all fields for the user table in db.
-type User struct {
+type RegisterUser struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required,min=8"`
+	Email    string `json:"email" validate:"required,email"`
+	Phone    string `json:"phone" validate:"required,min=10"`
+}
+
+type UserResponse struct {
 	ID            string    `json:"id"`
 	Username      string    `json:"username" validate:"required,min=3"`
 	Email         string    `json:"email" validate:"required,email"`
-	Password      string    `json:"password" validate:"required,min=8"`
 	Phone         string    `json:"phone" validate:"required,min=10"`
-	Role          string    `json:"role"`
+	Role          string    `json:"role" validate:"required,oneof=seller buyer admin"`
 	EmailVerified bool      `json:"email_verified,omitempty"`
 	PhoneVerified bool      `json:"phone_verified,omitempty"`
 	IsActive      bool      `json:"is_active,omitempty"`
 	LastActive    time.Time `json:"last_active"`
-	Model
+	CreatedAt     time.Time `json:"created_at"`
 }
