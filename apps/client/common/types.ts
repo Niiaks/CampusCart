f36f564a1332bcc38e4301c7d25c319117ede965
Niiaks <napappoe007@st.ug.edu.gh>
2026-02-16 23:14:36 +0000

    feat: add Brand, Listing, Category, CreateBrandRequest, and CreateListingRequest interfaces

--- apps/client/common/types.ts
+++ apps/client/common/types.ts
@@ -35,3 +35,65 @@ export interface User {
   last_active: string;
   created_at: string;
 }
+
+export interface Brand {
+  id: string;
+  seller_id: string;
+  name: string;
+  description: string;
+  profile_url?: string;
+  banner_url?: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Listing {
+  id: string;
+  brand_id: string;
+  category_id: string;
+  title: string;
+  description: string;
+  price: number;
+  image_url: string[];
+  video_url?: string[];
+  condition: "new" | "used" | "second-hand";
+  is_promoted: boolean;
+  is_discounted: boolean;
+  discount_percentage: number;
+  brand?: string;
+  model?: string;
+  size?: string;
+  storage_size?: string;
+  color?: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Category {
+  category_id: string;
+  name: string;
+  image_url: string;
+}
+
+export interface CreateBrandRequest {
+  name: string;
+  description: string;
+  profile_url?: string;
+  banner_url?: string;
+}
+
+export interface CreateListingRequest {
+  brand_id: string;
+  category_id: string;
+  title: string;
+  description: string;
+  price: number;
+  image_url: string[];
+  video_url?: string[];
+  condition: "new" | "used" | "second-hand";
+  brand?: string;
+  model?: string;
+  size?: string;
+  storage_size?: string;
+  color?: string;
+}
