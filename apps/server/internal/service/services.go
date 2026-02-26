4cdf94d298fdddce288ae7595803ad96e157a004
Niiaks <napappoe007@st.ug.edu.gh>
2026-02-26 21:21:50 +0000

    feat: update NewServices function to include file client for category service initialization

--- apps/server/internal/service/services.go
+++ apps/server/internal/service/services.go
@@ -1,19 +1,23 @@
 package service
 
 import (
+	"github.com/Niiaks/campusCart/internal/lib/file"
 	"github.com/Niiaks/campusCart/internal/lib/job"
 	"github.com/Niiaks/campusCart/internal/repository"
 )
 
 type Service struct {
-	Auth *AuthService
-	Job  *job.JobService
+	Auth     *AuthService
+	Job      *job.JobService
+	Category *CategoryService
 }
 
-func NewServices(repo *repository.Repository, job *job.JobService) *Service {
+func NewServices(repo *repository.Repository, job *job.JobService, file *file.Client) *Service {
 	authService := NewAuthService(repo.User, repo.Session, job)
+	categoryService := NewCategoryService(repo.Category, file)
 	return &Service{
-		Auth: authService,
-		Job:  job,
+		Auth:     authService,
+		Job:      job,
+		Category: categoryService,
 	}
 }
