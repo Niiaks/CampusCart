c88cba601fcf29f94d4347ce12dfb76ba3684cf1
Niiaks <napappoe007@st.ug.edu.gh>
2026-03-04 22:04:32 +0000

    feat: update NewHandlers to include Listing and Brand handlers

--- apps/server/internal/handler/handlers.go
+++ apps/server/internal/handler/handlers.go
@@ -10,13 +10,17 @@ type Handlers struct {
 	OpenAPI  *OpenAPIHandler
 	Auth     *AuthHandler
 	Category *CategoryHandler
+	Listing  *ListingHandler
+	Brand    *BrandHandler
 }
 
-func NewHandlers(s *server.Server, authService *service.AuthService, categoryService *service.CategoryService) *Handlers {
+func NewHandlers(s *server.Server, authService *service.AuthService, categoryService *service.CategoryService, listingService *service.ListingService, brandService *service.BrandService) *Handlers {
 	return &Handlers{
 		Health:   NewHealthHandler(s),
 		OpenAPI:  NewOpenAPIHandler(s),
 		Auth:     NewAuthHandler(s, authService),
 		Category: NewCategoryHandler(s, categoryService),
+		Listing:  NewListingHandler(s, listingService),
+		Brand:    NewBrandHandler(s, brandService),
 	}
 }
